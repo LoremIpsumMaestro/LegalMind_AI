54ef46bd583dfa6f6c313bcce7e04eec6257cfa4
LoremIpsumMaestro <maxime.dousset.pro@gmail.com>
2025-01-17 14:43:13 +0100

    Create basic homepage for Next.js application

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to LegalMind AI</h1>
+      <p>Your intelligent legal assistant is ready!</p>
+    </div>
+  );
+}

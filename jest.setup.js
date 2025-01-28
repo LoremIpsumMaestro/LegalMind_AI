43e8369b25f5bcd150d2d92fdeeff839c2d25999
LoremIpsumMaestro <maxime.dousset.pro@gmail.com>
2025-01-28 10:25:36 +0100

    Remove redundant jest.setup.js

--- jest.setup.js
+++
@@ -1,4 +0,0 @@
-import '@testing-library/jest-dom';
-
-global.process.env.NEXT_PUBLIC_SUPABASE_URL = 'https://cyjjgahyjvciaxhsxehm.supabase.co';
-global.process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY = 'your-anon-key';
